0fb1b62678b3433d749ef3a8ebd9a6e049808adb
Phalcon <alizna29@gmail.com>
2024-01-15 23:29:30 +0400

    conftest chages for compatibility with github actions

--- conftest.py
+++ conftest.py
@@ -38,7 +38,9 @@ def setup(request, browser, url):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     elif browser == "firefox":
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     elif browser == "edge":
