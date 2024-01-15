9affe0f3bcfab4d9c60cf70bd4d7391b48560a13
Phalcon <alizna29@gmail.com>
2024-01-15 17:42:05 +0400

    updates

--- utilities/utils.py
+++ utilities/utils.py
@@ -2,7 +2,6 @@ import csv
 import inspect
 import logging
 import softest
-from openpyxl import load_workbook
 
 
 class Utils(softest.TestCase):
@@ -17,20 +16,6 @@ class Utils(softest.TestCase):
         fh.setFormatter(formatter)
         logger.addHandler(fh)
         return logger
-
-    def read_data_from_excel(file_name: str, sheet):
-        datalist = []
-        wb = load_workbook(filename=file_name)
-        sh = wb[sheet]
-        row_ct = sh.max_row
-        col_ct = sh.max_column
-
-        for i in range(2, row_ct + 1):
-            row = []
-            for j in range(1, col_ct + 1):
-                row.append(sh.cell(row=i, column=j).value)
-            datalist.append(row)
-        return datalist
 
     def read_data_from_csv(filename: str):
         datalist = []
