89e56562a9fa5b8c46c7f435432eafab17f32979
Phalcon <alizna29@gmail.com>
2024-01-17 13:22:27 +0400

    additional logging

--- testcases/test_example.py
+++ testcases/test_example.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 import softest
 from utilities.utils import Utils
@@ -15,19 +14,30 @@ class TestExampleOne(softest.TestCase):
         self.ut = Utils()
         self.registration_page = RegistrationPage(self.driver)
 
+    @pytest.mark.run(order=1)
     def test_title(self):
-        assert "ssdg" in self.driver.title
+        # Check if the title of the page is correct
+        self.log.info("Verify the title")
+        assert "Welcome" in self.driver.title
+        self.log.info("Title verification complete")
 
-    # @data(("Anar", "+994551234567", "anar@mail.ru", "Azerbaijan", "Baku", "AnarM", "12345"))
-    # @unpack
+    @pytest.mark.run(order=2)
     @file_data("../testdata/testdata.json")
     def test_fill_and_submit_form(self, name, phone, email, country, city, username, password):
         # Step 1: Fill the registration form
+        self.log.info("Start filling the form.")
+        self.log.debug("Entering the name...")
         self.registration_page.enter_name(name)
+        self.log.debug("Entering the phone number...")
         self.registration_page.enter_phone(phone)
+        self.log.debug("Entering the email...")
         self.registration_page.enter_email(email)
+        self.log.debug("Selecting the country...")
         self.registration_page.select_country(country)
+        self.log.debug("Entering the city...")
         self.registration_page.enter_city(city)
+        self.log.debug("Entering the Username...")
         self.registration_page.enter_username(username)
+        self.log.debug("Entering the Password...")
         self.registration_page.enter_password(password)
-        self.log.info("Finished")
+        self.log.info("Finished filling the form")
